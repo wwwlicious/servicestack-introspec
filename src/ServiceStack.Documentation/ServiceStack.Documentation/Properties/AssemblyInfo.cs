3b12e2e81941f3d17e1d23ebca1d1b22f65c25dc
Donald Gray <donald.gray@maclean.co.uk>
2016-05-17 15:01:30 +0100

    Updated resource files

--- src/ServiceStack.Documentation/ServiceStack.Documentation/Properties/AssemblyInfo.cs
+++ src/ServiceStack.Documentation/ServiceStack.Documentation/Properties/AssemblyInfo.cs
@@ -1,14 +1,13 @@
 ﻿using System.Reflection;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("ServiceStack.Documentation")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Uses introspection to try and generate full documentation about service")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("MacLean Electrical")]
 [assembly: AssemblyProduct("ServiceStack.Documentation")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
