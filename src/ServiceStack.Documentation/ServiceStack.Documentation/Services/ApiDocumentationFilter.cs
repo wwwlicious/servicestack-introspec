dc4dab663ff126c9831bc6bde575f76563df9b62
Donald Gray <donald.gray@maclean.co.uk>
2016-05-26 09:06:19 +0100

    Extracted filtering logic to separate class and added interface for filterable props

---
+++ src/ServiceStack.Documentation/ServiceStack.Documentation/Services/ApiDocumentationFilter.cs
@@ -0,0 +1,54 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this 
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
+namespace ServiceStack.Documentation.Services
+{
+    using System;
+    using System.Linq;
+    using System.Linq.Expressions;
+    using Extensions;
+    using Linq;
+    using Models;
+
+    public class ApiDocumentationFilter
+    {
+        public static ApiDocumentation GetApiDocumentation(IFilterableSpecRequest request)
+        {
+            var apiSpecFeature = HostContext.GetPlugin<ApiSpecFeature>();
+            var documentation = apiSpecFeature.Documentation;
+
+            var filter = GetFilter(request);
+            if (filter != null)
+                documentation = documentation.CreateCopy(filter.Compile());
+            return documentation;
+        }
+
+        private static Expression<Func<ApiResourceDocumentation, bool>> GetFilter(IFilterableSpecRequest request)
+        {
+            bool hasFilter = false;
+
+            // The true predicate is just a starter to use as base
+            var predicate = PredicateBuilder.True<ApiResourceDocumentation>();
+            if (!request.DtoName.IsNullOrEmpty())
+            {
+                hasFilter = true;
+                predicate = predicate.And(doc => request.DtoName.Contains(doc.Title, StringComparer.OrdinalIgnoreCase));
+            }
+
+            if (!request.Tags.IsNullOrEmpty())
+            {
+                hasFilter = true;
+                predicate = predicate.And(doc => request.Tags.Any(t => doc.Tags.Contains(t, StringComparer.OrdinalIgnoreCase)));
+            }
+
+            if (!string.IsNullOrEmpty(request.Category))
+            {
+                hasFilter = true;
+                predicate = predicate.And(doc => string.Equals(request.Category, doc.Category, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return hasFilter ? predicate : null;
+        }
+    }
+}
--- src/ServiceStack.Documentation/ServiceStack.Documentation/Services/ApiSpecService.cs
+++ src/ServiceStack.Documentation/ServiceStack.Documentation/Services/ApiSpecService.cs
@@ -4,12 +4,7 @@
 
 namespace ServiceStack.Documentation.Services
 {
-    using System;
-    using System.Linq;
-    using System.Linq.Expressions;
     using DataAnnotations;
-    using Extensions;
-    using Linq;
     using Models;
 
     public class ApiSpecService : Service
@@ -17,54 +12,16 @@ namespace ServiceStack.Documentation.Services
         public object Get(SpecRequest request)
         {
             // Get the filtered documentation to return
-            var documentation = GetApiDocumentation(request);
+            var documentation = ApiDocumentationFilter.GetApiDocumentation(request);
 
             // TODO Filter out by auth permissions
             return new SpecResponse { ApiDocumentation = documentation };
-        }
-
-        private static ApiDocumentation GetApiDocumentation(SpecRequest request)
-        {
-            var apiSpecFeature = HostContext.GetPlugin<ApiSpecFeature>();
-            var documentation = apiSpecFeature.Documentation;
-
-            var filter = GetFilter(request);
-            if (filter != null)
-                documentation = documentation.CreateCopy(filter.Compile());
-            return documentation;
-        }
-
-        private static Expression<Func<ApiResourceDocumentation, bool>> GetFilter(SpecRequest request)
-        {
-            bool hasFilter = false;
-
-            // The true predicate is just a starter to use as base
-            var predicate = PredicateBuilder.True<ApiResourceDocumentation>();
-            if (!request.DtoName.IsNullOrEmpty())
-            {
-                hasFilter = true;
-                predicate = predicate.And(doc => request.DtoName.Contains(doc.Title, StringComparer.OrdinalIgnoreCase));
-            }
-
-            if (!request.Tags.IsNullOrEmpty())
-            {
-                hasFilter = true;
-                predicate = predicate.And(doc => request.Tags.Any(t => doc.Tags.Contains(t, StringComparer.OrdinalIgnoreCase)));
-            }
-
-            if (!string.IsNullOrEmpty(request.Category))
-            {
-                hasFilter = true;
-                predicate = predicate.And(doc => string.Equals(request.Category, doc.Category, StringComparison.OrdinalIgnoreCase));
-            }
-
-            return hasFilter ? predicate : null;
         }
     }
 
     [Route(Constants.SpecUri)]
     [Exclude(Feature.Metadata | Feature.ServiceDiscovery)]
-    public class SpecRequest : IReturn<SpecResponse>
+    public class SpecRequest : IReturn<SpecResponse>, IFilterableSpecRequest
     {
         public string[] DtoName { get; set; }
         public string Category { get; set; }
